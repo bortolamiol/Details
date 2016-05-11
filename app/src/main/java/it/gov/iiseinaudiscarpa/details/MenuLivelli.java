138eb4f086b7ad48d29ad6a3b3f51f1629a9eebd
bortolamiol alessandro <bortolamiolalessandr@itseinaudi.local>
2016-05-11 08:32:06 +0200

    Initial commit

---
+++ app/src/main/java/it/gov/iiseinaudiscarpa/details/MenuLivelli.java
@@ -0,0 +1,16 @@
+package it.gov.iiseinaudiscarpa.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MenuLivelli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_livelli);
+        Button btn1 = (Button) findViewById(R.id.button2);
+
+    }
+}
